c48f782484550ff24d67913670e8fa7f49a332aa
Victor Leandro <83599460+Afrontoso@users.noreply.github.com>
2025-07-10 16:16:07 +0000

    add ID de materias

---
+++ scripts/materias.js
@@ -0,0 +1,10 @@
+// scripts/materias.js
+
+const dadosFluxograma = {
+    "1": [
+        { id: "C1", nome: "Cálculo 1", codigo: "MAT001", creditos: 6, prerequisitos: [] },
+        { id: "EA", nome: "Engenharia e Ambiente", codigo: "ENG001", creditos: 4, prerequisitos: [] },
+        { id: "IE", nome: "Introdução à Engenharia", codigo: "ENG002", creditos: 4, prerequisitos: [] },
+    ]
+    
+};
